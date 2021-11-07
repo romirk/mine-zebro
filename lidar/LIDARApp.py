06e854029073eafa414babaed59b23dc7b952420
Marijn Adriaanse <eismquantum2@gmail.com>
2021-11-07 10:47:49 +0100

    Created basis for lidarapp

---
+++ lidar/LIDARApp.py
@@ -0,0 +1,141 @@
+
+"""
+
+LIDAR App (might use I2C idk
+
+
+
+commands
+
+read
+    read values
+assertsafe
+    read values and check
+assertsafe m/mid/middle:<distance> h/half/halfway:<distance> s/side:<distance>
+    like assertsafe but with values
+
+"""
+
+
+from time import sleep
+import traceback
+
+
+DEFAULT_DIST_MID=17
+DEFAULT_DIST_HALF=20
+DEFAULT_DIST_SIDE=20
+
+
+
+class LIDARApp:
+    def __init__(self,bus,returnfunc,checkhaltfunc):
+        self.bus=bus
+        self.returnf=returnfunc #function for sending back data and errors
+        self.checkhalt=checkhaltfunc #function to check halt flag. returns True if the robot should stop
+
+        self.distances=[0,0,0,0,0]#left to right (0=left)
+
+
+    #function for executing commands
+    def execute(self,command):
+        command=command.split() #commands consist of terms separated by spaces
+        func,args=None,[]
+
+        try: #in case of an error, report back the exact error
+            if not len(command):    return self.returnf(self._invalid_command("No command specified"))
+            if command[0]=="read": #read values and report back
+                if len(command)>1:
+                    return self.returnf(self._invalid_command("Command 'read' takes no arguments"))
+                func=self.read
+                args=[]
+
+            elif command[0]=="assertsafe":
+                args=[DEFAULT_DIST_MID,DEFAULT_DIST_HALF,DEFAULT_DIST_SIDE]
+                for term in command[1:]:
+                    try:
+                        key,val=term.split(":")
+                    except:
+                        self.returnf(self._invalid_command("Invalid argument: '%s'"%term))
+                        return
+                    
+                    try:
+                        val=int(val)
+                        assert val>0
+                    except:
+                        self.returnf(self._invalid_command("Invalid distance: '%s'"%str(val)))
+                        return
+
+                    if key in ("m","mid","middle","c","centre","center"):
+                        args[0]=val
+                    elif key in ("h","half","halfway"):
+                        args[1]=val
+                    elif key in ("s","side","o","out","outer"):
+                        args[2]=val
+                    else:
+                        self.returnf(self._invalid_command("Invalid direction: '%s'"%key))
+                        return 
+                
+            else:
+                self.returnf( self._invalid_command("No valid command specified") )
+                return
+
+        except:
+            self.returnf( self._invalid_command("Exception occured while reading command:\n%s"%traceback.format_exc()) )
+            return
+
+        try: #try to execute command. send back traceback if it fails (which might result in a dangerous situation for the rover!)
+            func(args)
+        except:
+            self.returnf( self._error("Exception occured during execution of command:\n%s"%traceback.format_exc()) )
+            return
+
+    #some functions for reoccuring results
+    def _halt(self):
+        return self._error("Execution halted by TRON")
+    def _invalid_command(self,err=""):
+        if err:
+            return self._error("Invalid command: %s"%err)
+        return self._error("Invalid command")
+    
+    def _error(self,err="",data={}):
+        return ((1,err),data)
+    def _warning(self,err="",data={}):
+        return ((2,err),data)
+    def _data(self,data):
+        return (0,data)
+    def _info(self,msg):
+        return ((0,msg),{})
+
+    def read(self,args):
+        pass#I don't know how the lidar works yet
+        #write to self.distances
+
+
+        self.returnf(self._data(dict(zip(["l","hl","m","hr","r"],self.distances))))
+    
+    def assertsafe(self,args):
+        self.read()
+
+        if self.distances[2]<args[0] or min(self.distances[1::2])<args[1] or min(self.distances[0::4])<args[2]:
+            self.returnf(self._error("Object detected within close range"))
+            return
+        self.returnf(self._info("No object detected within close range"))
+
+
+     
+if __name__=="__main__":
+    from pprint import pprint
+
+    from smbus2 import SMBus
+
+    bus=SMBus(1) #create bus
+    
+    
+    lidar=LIDARApp(bus,pprint,int)
+    print("LIDAR app testing environment - enter commands or 'exit'")
+    while True:#(i:=input("> "))!="exit":#walrus operator is python 3.8, pi runs 3.7
+        i=input("> ").strip()
+        if i=="exit":
+            break
+        if i:
+            lidar.execute(i)
