c618a86fb4be51c8d92bb7bd21fc44e9d0a8411f
Marijn Adriaanse <eismquantum2@gmail.com>
2021-11-30 21:44:44 +0100

    Changed ADS address and voltage divider value

--- shared_ads.py
+++ shared_ads.py
@@ -4,5 +4,5 @@ i2c=busio.I2C(board.SCL,board.SDA)
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn as Channel
 
-ads=ADS.ADS1115(i2c)
+ads=ADS.ADS1115(i2c,address=0x49)
 ads.gain=1
